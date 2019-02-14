dfe3311251bcad21e347955cbcab669f72903263
mohamilr <ibrahimdamy7@gmail.com>
2019-02-14 00:29:47 -0800

    use bodyparser library

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 9001;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
     return res.send('the app is working');
