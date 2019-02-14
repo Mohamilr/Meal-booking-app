157d014e3cb7ec0dacecac5216459b709e0d36a4
mohamilr <ibrahimdamy7@gmail.com>
2019-02-14 00:28:09 -0800

    meal controller

---
+++ api/utils/dummyData.js
@@ -0,0 +1,28 @@
+export default{
+    meals: [
+        {
+            id: 1,
+            name: "Semo",
+            size :"medium",
+            price: '500'
+        },
+        {
+            id: 2,
+            name: "Semo",
+            size :"medium",
+            price: '500'
+        },
+        {
+            id: 3,
+            name: 'Jollof Rice',
+            size: 'plates',
+            price: '450'
+        },
+        {
+            id: 4,
+            name: "Semo",
+            size :"medium",
+            price: '300'
+        }
+    ]
+}
